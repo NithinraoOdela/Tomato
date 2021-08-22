1fdccac2bd910ca59df66c3a92685bfc750939ea
NithinraoOdela <65495762+NithinraoOdela@users.noreply.github.com>
2021-08-22 22:01:17 +0530

    Add files via upload

---
+++ src/components/Navbar/styles.js
@@ -0,0 +1,112 @@
+import { pink } from '@material-ui/core/colors';
+import { makeStyles, fade } from '@material-ui/core/styles';
+
+const drawerWidth = 0;
+
+export default makeStyles((theme) => ({
+  appBar: {
+    boxShadow: 'none',
+    borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
+    },
+  },
+  title: {
+    flexGrow: 1,
+    alignItems: 'center',
+    display: 'flex',
+    textDecoration: 'none',
+  },
+  image: {
+    marginRight: '10px',
+
+  },
+  imageq: {
+    fontSize: '23px',
+    color: 'orange'
+
+  },
+  imagew: {
+    fontSize: '34px',
+    color: 'Blue'
+
+  },
+  imagee: {
+    fontSize: '48px',
+    color: 'Green'
+
+  },
+  imagees: {
+    fontSize: '23px',
+    color: 'white',
+    backgroundColor: 'black',
+    borderRadius: '4px',
+
+  },
+  imageess: {
+    fontSize: '20px',
+    color: 'white',
+    backgroundColor: 'green',
+    borderRadius: '4px',
+    // marginLeft: "30px",
+
+  },
+  imageesss: {
+    fontSize: '20px',
+    color: 'white',
+    backgroundColor: 'green',
+    borderRadius: '4px',
+    marginRight: "20px",
+
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+  grow: {
+    flexGrow: 1,
+  },
+
+
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+    
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
+
